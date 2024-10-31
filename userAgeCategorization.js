c956f265175efd3ce5139d5a74344f8a55643d86
Jean Matbagan <matbaganjean@gmail.com>
2024-10-31 21:56:26 +0800

    Add files via upload

---
+++ userAgeCategorization.js
@@ -0,0 +1,24 @@
+// Check if the age is 5 or less
+if (age <= 5) {
+    console.log("You are a toddler");
+} 
+// Check if the age is between 6 and 12
+else if (age >= 6 && age <= 12) {
+    console.log("You are a child");
+} 
+// Check if the age is between 13 and 19
+else if (age >= 13 && age <= 19) {
+    console.log("You are a teenager");
+} 
+// Check if the age is between 20 and 35
+else if (age >= 20 && age <= 35) {
+    console.log("You are a young adult");
+} 
+// Check if the age is between 36 and 60
+else if (age >= 36  && age <= 60) {
+    console.log("You are middle-aged");
+} 
+// If age is greater than 60
+else {
+    console.log("You are a senior");
+}
